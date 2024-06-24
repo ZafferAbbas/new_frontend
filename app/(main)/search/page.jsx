26b9a37313f988dc91ac43131456efb587ad5639
ZafferAbbas <104623896+ZafferAbbas@users.noreply.github.com>
2024-06-24 03:51:19 -0400

    Update page.jsx

--- app/(main)/search/page.jsx
+++ app/(main)/search/page.jsx
@@ -9,11 +9,13 @@ import { SwiperSlide } from "swiper/react";
 import { getAllUserProducts, liveSearch } from "@/lib/services/product";
 import Link from "next/link";
 import withAuth from "@/lib/withAuth";
+import Loader from "@/components/loader";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
   const [products, setProducts] = useState([]);
   const [allProducts, setAllProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // Debounce function to delay the API call
   const debounce = (func, delay) => {
@@ -39,6 +41,8 @@ function Home() {
       }
     } catch (error) {
       console.error("Fetch Error:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -73,54 +77,57 @@ function Home() {
 
   return (
     <div className="main-container">
-      <SideMenu />
-      <div className="overflow-hidden">
-        <TopNavbar />
-        <MainContent>
-          <div className="container">
-            <div className="grid grid-cols-1 md:grid-cols-3 text-white gap-y-6 md:gap-x-6 mb-6">
-              <div className="col-span-2 bg-[#060B28] rounded-[20px] p-6">
-                <p className="text-xs mb-3">Search for Product</p>
-                <input
-                  className="w-full text-[#e1e1e1] bg-[#0F1535] rounded-2xl border border-[#6271c2] px-4 py-3 text-xs"
-                  placeholder="hazy"
-                  value={searchQuery}
-                  onChange={handleSearchChange}
-                />
-              </div>
-              <div className="bg-[#060B28] rounded-[20px] p-6">
-                <p className="text-lg font-bold mb-3">Can't find what you're looking for?</p>
-                <Link href="/edit">
-                  <div className="mx-auto bg-white rounded-xl w-[100px] h-8 grid place-items-center text-[10px] text-[#0F1535] cursor-pointer">
-                    Add
+      {loading && <Loader />}
+      {!loading && (
+        <>
+          <SideMenu />
+          <div className="overflow-hidden">
+            <TopNavbar />
+            <MainContent>
+              <div className="container">
+                <div className="grid grid-cols-1 md:grid-cols-3 text-white gap-y-6 md:gap-x-6 mb-6">
+                  <div className="col-span-2 bg-[#060B28] rounded-[20px] p-6">
+                    <p className="text-xs mb-3">Search for Product</p>
+                    <input
+                      className="w-full text-[#e1e1e1] bg-[#0F1535] rounded-2xl border border-[#6271c2] px-4 py-3 text-xs"
+                      placeholder="hazy"
+                      value={searchQuery}
+                      onChange={handleSearchChange}
+                    />
                   </div>
-                </Link>
+                  <div className="bg-[#060B28] rounded-[20px] p-6">
+                    <p className="text-lg font-bold mb-3">Can't find what you're looking for?</p>
+                    <Link href="/edit">
+                      <div className="mx-auto bg-white rounded-xl w-[100px] h-8 grid place-items-center text-[10px] text-[#0F1535] cursor-pointer">
+                        Add
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                  {products.map((product) => {
+                    const formattedItems = product?.pricintList?.map((item) => `${item.size}oz - ${item.price}$`);
+                    return (
+                      <SwiperSlide key={product._id}>
+                        <ProductCard
+                          recordId={product._id}
+                          bgImgSrc={product.imageUrl}
+                          num={product.quantity}
+                          title={product.productName}
+                          description={product.description}
+                          members={formattedItems}
+                        />
+                      </SwiperSlide>
+                    );
+                  })}
+                </div>
               </div>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {products.map((product) => {
-                const formattedItems = product?.pricintList?.map((item) => `${item.size}oz - ${item.price}$`);
-                return (
-					<SwiperSlide>
-                  <ProductCard
-                    key={product._id}
-                    recordId={product._id}
-                    bgImgSrc={product.imageUrl}
-                    num={product.quantity}
-                    title={product.productName}
-                    description={product.description}
-                    members={formattedItems}
-                  />
-				  </SwiperSlide>
-                );
-              })}
-            </div>
+            </MainContent>
           </div>
-        </MainContent>
-      </div>
+        </>
+      )}
     </div>
   );
 }
 
 export default withAuth(Home);
-// export default Home;
